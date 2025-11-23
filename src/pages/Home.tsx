7019fae0de3256256e1fce1084202d6363277b93
Marcos Iglesias <markkos89@gmail.com>
2025-11-23 10:06:16 +0800

    feat: loaded logo on home screen and imported new fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
-
+import logo from '/logo.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2 '>
+      <img src={logo} alt='InteropSafe Logo' width={350} />
+      <h1 className='font-timmana'>Create an InteropSAFE</h1>
+      <h3 className='font-tilt-neon'>Same Safe Address on multiple chains with one click</h3>
+    </div>
   )
 }
